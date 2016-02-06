1ab9fcda96a871caaae82bec40b06a355d137f5f
Brian Masney <masneyb@onstation.org>
2016-02-06 09:22:01 -0500

    code formatting to comply with the Linux kernel coding standards

--- humiture.c
+++ humiture.c
@@ -1,7 +1,26 @@
-/* Most of this code came from
+/*
+ * humiture.c - Read temperature and humidity levels from a DHT11 or
+ *              DHT22 sensor.
+ *
+ * Copyright (C) 2015 Cavon Lee
  * https://github.com/sunfounder/SunFounder_SensorKit_for_RPi2/blob/master/C/28_humiture/humiture.c
- * Modified by Brian Masney <masneyb@onstation.org> to work against a DHT22 sensor.
- * Also added more bounds checking.
+ *
+ * Copyright (C) 2016 Brian Masney <masneyb@onstation.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
  */
 
 #include <wiringPi.h>
@@ -18,15 +37,18 @@ struct temperature_humidity {
 	float humidity;
 };
 
-float celsius_to_fahrenheit(float input) {
+float celsius_to_fahrenheit(float input)
+{
 	return input * 9.0 / 5.0 + 32;
 }
 
-float celsius_to_celsius(float input) {
+float celsius_to_celsius(float input)
+{
 	return input;
 }
 
-float celsius_to_kelvin(float input) {
+float celsius_to_kelvin(float input)
+{
 	return input + 273.15;
 }
 
@@ -41,15 +63,17 @@ void dht22_parse_data(int data[5], struct temperature_humidity *result)
 	result->humidity = (data[0] * 256 + data[1]) / 10;
 
 	result->temperature = ((data[2] & 0x7F) * 256 + data[3]) / 10;
-	if (data[2] & 0x80) {
+	if (data[2] & 0x80)
 		result->temperature *= -1;
-	}
 }
 
-int read_data(int gpioPin, void (*parser)(int data[5], struct temperature_humidity *result),
-		float (*temperature_converter)(float input), char temperature_unit)
+int read_data(int gpioPin, void (*parser)(int data[5],
+		struct temperature_humidity *result),
+		float (*temperature_converter)(float input),
+		char temperature_unit)
 {
 	int data[5];
+	int checksum;
 	uint8_t laststate = HIGH;
 	uint8_t counter = 0;
 	uint8_t j = 0, i;
@@ -63,52 +87,53 @@ int read_data(int gpioPin, void (*parser)(int data[5], struct temperature_humidi
 
 	/* then pull it up for 40 microseconds */
 	digitalWrite(gpioPin, HIGH);
-	delayMicroseconds(40); 
+	delayMicroseconds(40);
 
 	/* prepare to read the pin */
 	pinMode(gpioPin, INPUT);
 
 	/* detect change and read data */
-	for (i=0; i < MAXTIMINGS; i++) {
+	for (i = 0; i < MAXTIMINGS; i++) {
 		counter = 0;
 		while (digitalRead(gpioPin) == laststate) {
 			counter++;
 			delayMicroseconds(1);
-			if (counter == 255) {
+			if (counter == 255)
 				break;
-			}
 		}
 		laststate = digitalRead(gpioPin);
 
-		if (counter == 255) {
+		if (counter == 255)
 			break;
-		}
 
 		/* ignore first 3 transitions */
 		if ((i >= 4) && (i%2 == 0)) {
 			/* shove each bit into the storage bytes */
 			data[j/8] <<= 1;
-			if (counter > 16) {
+			if (counter > 16)
 				data[j/8] |= 1;
-			}
 			j++;
 		}
 	}
 
 	/* check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
-	   print it out if data is good */
-	if ((j >= 40) && 
-			(data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) ) {
+	 * print it out if data is good
+	 */
+
+	checksum = (data[0] + data[1] + data[2] + data[3]) & 0xFF;
+	if (j >= 40 && data[4] == checksum) {
 		struct temperature_humidity result;
 
 		parser(data, &result);
-		if (result.humidity < 0.0 || result.humidity > 100.0 ||
-			result.temperature < -70.0 || result.temperature > 70.0) {
+		if (result.humidity < 0.0 ||
+			result.humidity > 100.0 ||
+			result.temperature < -70.0 ||
+			result.temperature > 70.0) {
 			/* Bad reading */
 			return 0;
 		}
 
-		printf("Humidity: %.1f %%\nTemperature: %.1f *%c\n", 
+		printf("Humidity: %.1f %%\nTemperature: %.1f *%c\n",
 				result.humidity,
 				temperature_converter(result.temperature),
 				temperature_unit);
@@ -119,66 +144,57 @@ int read_data(int gpioPin, void (*parser)(int data[5], struct temperature_humidi
 	return 0;
 }
 
-void usage() {
+void usage(void)
+{
 	printf("usage: humiture <GPIO pin #> <sensor> <temperature>\n");
 	printf("\t- valid sensor values: dht22\n");
 	printf("\t- valid temperature values: celsius, fahrenheit, kelvin\n");
 	exit(1);
 }
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
+	void (*parser)(int data[5], struct temperature_humidity *result);
+	float (*temperature_converter)(float input);
+	char temperature_unit;
 	int gpioPin;
 
-	if (argc != 4) {
+	if (argc != 4)
 		usage();
-	}
 
 	errno = 0;
 	gpioPin = strtol(argv[1], NULL, 10);
-	if (errno != 0) {
+	if (errno != 0)
 		usage();
-	}
 
-	void (*parser)(int data[5], struct temperature_humidity *result);
-	if (strcmp(argv[2], "dht11") == 0) {
+	if (strcmp(argv[2], "dht11") == 0)
 		parser = &dht11_parse_data;
-	}
-	else if (strcmp(argv[2], "dht22") == 0) {
+	else if (strcmp(argv[2], "dht22") == 0)
 		parser = &dht22_parse_data;
-	}
-	else {
+	else
 		usage();
-	}
 
-	char temperature_unit;
-	float (*temperature_converter)(float input);
 	if (strcmp(argv[3], "celsius") == 0) {
 		temperature_converter = &celsius_to_celsius;
 		temperature_unit = 'C';
-	}
-	else if (strcmp(argv[3], "fahrenheit") == 0) {
+	} else if (strcmp(argv[3], "fahrenheit") == 0) {
 		temperature_converter = &celsius_to_fahrenheit;
 		temperature_unit = 'F';
-	}
-	else if (strcmp(argv[3], "kelvin") == 0) {
+	} else if (strcmp(argv[3], "kelvin") == 0) {
 		temperature_converter = &celsius_to_kelvin;
 		temperature_unit = 'K';
-	}
-	else {
+	} else
 		usage();
-	}
 
 
-	if (wiringPiSetup () == -1) {
-		return (1) ;
-	}
+	if (wiringPiSetup() == -1)
+		return 1;
 
 	while (!read_data(gpioPin, parser, temperature_converter,
 				temperature_unit)) {
 		/* NOOP */
 	}
 
-	return (0);
+	return 0;
 }
 
